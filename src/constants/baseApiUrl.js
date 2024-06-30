e5446a1d085c6a7dc5118ed96f51d847527c9070
jinyus <jinyhehe@gmail.com>
2024-06-30 20:00:10 +0900

    fix: baseApiUrl 수정

--- src/constants/baseApiUrl.js
+++ src/constants/baseApiUrl.js
@@ -1,2 +1,2 @@
-// export const baseApiUrl = "https://spacestory.duckdns.org";
-export const baseApiUrl = "http://localhost:8080";
+export const baseApiUrl = "https://spacestory.duckdns.org";
+// export const baseApiUrl = "http://localhost:8080";
